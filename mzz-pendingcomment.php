b7996ecd93c7f08d7023bd8291ef2e6201f04f85
mjassen <mjassen@users.noreply.github.com>
2014-09-18 20:34:57 -0400

    Update mzz-pendingcomment.php

--- mzz-pendingcomment.php
+++ mzz-pendingcomment.php
@@ -1,20 +1,30 @@
 <?php
+
  /**
-* Plugin Name: mzz pendingcomment
-* Plugin URI: http://wieldlinux.com/
-* Description: plugin sets collabpress comments to pending
-* Version: v0.1
+* Plugin Name: Mzz Pendingcomment
+* Plugin URI: https://github.com/mjassen/mzz-pendingcomment
+* Description: Helper plugin for CollabPress sets collabpress comments to pending.
+* Version: 0.0.1
 * Author: mjassen
 * Author URI: http://wieldlinux.com/
 * License: MIT
 */
- 
-function mzz_cp_pendingcomment( $approved , $commentdata )
-{
-  // if the comment_type found in $commentdata is 'collabpress', then set approval to 0 (pending), otherwise leave its approval to $approved(i.e. leave its approval alone)
-        return ( $commentdata['comment_type'] == 'collabpress' ) ? '0' : $approved;
 
+//tell wordpress to execute the mzz_comment_inserted function whenever it executes the wp_insert_comment function-- for example when collabpress calls the wp_insert_comment function.
+add_action('wp_insert_comment','mzz_comment_inserted',99,2);
+
+function mzz_comment_inserted($comment_id, $comment_object) {
+
+	//for the comment that just got inserted, check whether the comment type is 'collabpress'
+    if ($comment_object->comment_type == 'collabpress') {
+        
+        //the comment type was seen to be collabpress. It was already inserted into the database. Now we go back into the database and update that comment's approved status to 0 (pending)
+        $mzzcommentarr = array();
+		$mzzcommentarr['comment_ID'] = $comment_id;
+		$mzzcommentarr['comment_approved'] = 0;
+		wp_update_comment( $mzzcommentarr );
+
+    }
 }
 
-add_filter( 'pre_comment_approved' , 'mzz_cp_pendingcomment' , '99', '2' );
 ?>
